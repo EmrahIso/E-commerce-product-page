883832c091cef54ebcaf2d6a3c1ea2f3839fd641
Emrah Isovic <emrahisovic20@gmail.com>
2024-01-05 18:46:29 +0100

    Added functionality to quantity conter

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+//CountQuantity
+const minusBtnEl = document.querySelector('.product__minus-icon');
+const plusBtnEl = document.querySelector('.product__plus-icon');
+const quantityNumberEl = document.querySelector('.product__info-quantity-number');
+let quantityNumberValue = +quantityNumberEl.textContent;
+console.log(+quantityNumberEl.textContent);
+
+function minusOneQuantity() {
+    if(quantityNumberValue <= 0) {
+      return false;
+    } else  {
+      quantityNumberValue -= 1;
+      quantityNumberEl.textContent = quantityNumberValue;
+    }
+}
+
+function plusOneQuantity() {
+    if(quantityNumberValue >= 100) { 
+      return false
+    } else  {
+      quantityNumberValue += 1;
+      quantityNumberEl.textContent = quantityNumberValue;
+    }
+}
+
+minusBtnEl.addEventListener('click', minusOneQuantity);
+plusBtnEl.addEventListener('click', plusOneQuantity);
