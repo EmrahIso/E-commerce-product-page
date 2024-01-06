21efb331604efb59b442fdbc1bc7387fb85b8ca3
Emrah Isovic <emrahisovic20@gmail.com>
2024-01-06 19:58:57 +0100

    Functionality to open and close lightbox popup

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const minusBtnEl = document.querySelector('.product__minus-icon');
 const plusBtnEl = document.querySelector('.product__plus-icon');
 const quantityNumberEl = document.querySelector('.product__info-quantity-number');
 let quantityNumberValue = +quantityNumberEl.textContent;
-console.log(+quantityNumberEl.textContent);
 
 function minusOneQuantity() {
     if(quantityNumberValue <= 0) {
@@ -25,3 +24,28 @@ function plusOneQuantity() {
 
 minusBtnEl.addEventListener('click', minusOneQuantity);
 plusBtnEl.addEventListener('click', plusOneQuantity);
+
+//Lightbox
+const rootEl = document.querySelector('html');
+const productPreviewBigContEl = document.querySelector('.product__preview-big-cont');
+const productPreviewSmallBoxEls = document.querySelectorAll('.product__preview-img-small-box');
+
+productPreviewSmallBoxEls.forEach(productPreviewSmallBoxEl=> {
+    productPreviewSmallBoxEl.addEventListener('click', showLightboxPopup);
+})
+
+productPreviewBigContEl.addEventListener('click', showLightboxPopup);
+
+function showLightboxPopup() {
+  rootEl.classList.add('lightbox');
+}
+
+//Close Lightbox
+
+const closeLightboxBtnEl = document.querySelector('.popup__close-btn');
+
+closeLightboxBtnEl.addEventListener('click', closeLightboxPopup);
+
+function closeLightboxPopup() {
+  rootEl.classList.remove('lightbox');
+}
